51d0642f6a846470649b2a94d2881e007b907ea3
sureshkumarpgcp <sureshkumarpgcp@gmail.com>
2021-02-14 21:26:53 +0100

    updated the region and the ami ids

--- terraform/database/main.tf
+++ terraform/database/main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-    region = "us-east-1"
+    region = "us-east-2"
 }
 
 resource "aws_instance" "devops_01" {
-    ami = "ami-04b9e92b5572fa0d1"
+    ami = "ami-0996d3051b72b5b2c"
     instance_type = "t2.micro"
-    key_name = "devops"
+    key_name = "crosstown"
 
     tags = {
         Name = "db-instance"

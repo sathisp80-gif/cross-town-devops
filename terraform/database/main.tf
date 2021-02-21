49c530b44850ad079faed3c36fae93511baa53c5
sathisp80-gif <78876248+sathisp80-gif@users.noreply.github.com>
2021-02-21 13:03:35 -0600

    r53 fix

--- terraform/database/main.tf
+++ terraform/database/main.tf
@@ -26,12 +26,14 @@ resource "aws_route53_record" "database_testondemand_ga_zone_ns_record" {
 zone_id = "${aws_route53_zone.testondemand_com.Z07954263O9R4Y0C94383}"
 name    = "database"
 type    = "NS"
-records = ["ns-477.awsdns-59.com", "ns-563.awsdns-06.net"] ttl     = "86400" 
+records = ["ns-477.awsdns-59.com", "ns-563.awsdns-06.net"] 
+ttl = "86400" 
 } 
 resource "aws_route53_record" "database_testondemand_ga_zone_default_ns_record" { 
 zone_id = "${aws_route53_zone.testondemand_com.Z07954263O9R4Y0C94383}" 
 type    = "NS" 
 name    = "database.testondemand.ga" 
-records = ["ns-477.awsdns-59.com.", "ns-563.awsdns-06.net."] ttl     = "86400" 
+records = ["ns-477.awsdns-59.com.", "ns-563.awsdns-06.net."] 
+ttl     = "86400" 
 }
 

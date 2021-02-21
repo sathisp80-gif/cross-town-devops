6d076c9fedfc76a721b44a576e730cff0835b638
sathisp80-gif <78876248+sathisp80-gif@users.noreply.github.com>
2021-02-21 13:00:25 -0600

    route53

--- terraform/database/main.tf
+++ terraform/database/main.tf
@@ -13,16 +13,25 @@ resource "aws_instance" "devops_01" {
 }
 
 
-resource "aws_route53_zone" "testondemand" {
-    name = "testondemand.ga"
-    comment =""
-  }
+resource "aws_route53_zone" "database_testondemand_ga" {
+name          = "database.testondemand.ga."
+comment       = "Managed by Terraform, NS delegate for third party provider"
+force_destroy = false
+tags {
+ManagedBy = "Terraform"
+}
+}
 
-resource "aws_route53_record" "database" {
-zone_id = aws_route53_zone.testondemand.Z07954263O9R4Y0C94383
-name = "database.testondemand.ga"
-type = "A"
-ttl = "300"
-records = [devops_01.database.public_ip]
+resource "aws_route53_record" "database_testondemand_ga_zone_ns_record" {
+zone_id = "${aws_route53_zone.testondemand_com.Z07954263O9R4Y0C94383}"
+name    = "database"
+type    = "NS"
+records = ["ns-477.awsdns-59.com", "ns-563.awsdns-06.net"] ttl     = "86400" 
+} 
+resource "aws_route53_record" "database_testondemand_ga_zone_default_ns_record" { 
+zone_id = "${aws_route53_zone.testondemand_com.Z07954263O9R4Y0C94383}" 
+type    = "NS" 
+name    = "database.testondemand.ga" 
+records = ["ns-477.awsdns-59.com.", "ns-563.awsdns-06.net."] ttl     = "86400" 
 }
 
